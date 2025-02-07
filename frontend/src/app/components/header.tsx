481b4ac8acf340cee1b99981b683ded5493e98cf
Aegis-07 <power__dev0724@outlook.com>
2025-02-06 16:46:18 -0800

    header bug

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -27,31 +27,32 @@ const Header = () => {
               className='cursor-pointer'
             />
           </div>
-          {!token && (
+          {!token ? (
             <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
               <a href="/login" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Login</a>
               <a href="/register" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a>
             </div>
-          )}
-          {token && roles.includes("client") ? (
-            <>
-              <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-                <div className='mr-5'>
-                  <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">New Job</a>
-                  <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">My Jobs</a>
-                </div>
-                <Profile />
-              </div>
-            </>
           ) : (
-            <>
-              <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-                <div className='mr-5'>
-                  <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">All Jobs</a>
+            roles.includes("client") ? (
+              <>
+                <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+                  <div className='mr-5'>
+                    <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">New Job</a>
+                    <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">My Jobs</a>
+                  </div>
+                  <Profile />
                 </div>
-                <Profile />
-              </div>
-            </>
+              </>
+            ) : (
+              <>
+                <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+                  <div className='mr-5'>
+                    <a href="#" className="p-2 lg:px-4 md:mx-2 text-green-600 text-center border border-solid border-green-600 rounded hover:bg-green-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">All Jobs</a>
+                  </div>
+                  <Profile />
+                </div>
+              </>
+            )
           )}
         </div>
       </nav>
