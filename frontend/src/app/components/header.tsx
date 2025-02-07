6bbaa12abf1f8ab4d191409fa325706666566e04
Aegis-07 <power__dev0724@outlook.com>
2025-02-06 22:36:24 -0800

    add footer

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -16,7 +16,6 @@ const Header = () => {
     <div className="h-25 bg-[#1B1AFF]">
       <nav className="py-2 md:py-4">
         <div className="container px-4 mx-auto md:flex md:items-center">
-
           <div className="flex justify-between items-center">
             <Image
               src="/logo.webp"
@@ -29,16 +28,16 @@ const Header = () => {
           </div>
           {!token ? (
             <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-              <a href="/login" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-[#12D465] rounded hover:bg-[#F0F4F8] hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Login</a>
-              {/* <a href="/register" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-[#12D465] rounded hover:bg-[#F0F4F8] hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a> */}
+              <a href="/login" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-green-500 rounded hover:bg-gray-100 hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Login</a>
+              {/* <a href="/register" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-green-500 rounded hover:bg-gray-100 hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a> */}
             </div>
           ) : (
             roles.includes("client") ? (
               <>
                 <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
                   <div className='mr-5'>
-                    <a href="#" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-[#12D465] rounded hover:bg-[#F0F4F8] hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">New Job</a>
-                    <a href="#" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-[#12D465] rounded hover:bg-[#F0F4F8] hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">My Jobs</a>
+                    <a href="/dashboard/client" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-green-500 rounded hover:bg-gray-100 hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">New Job</a>
+                    <a href="/dashboard/client/jobList" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-green-500 rounded hover:bg-gray-100 hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">My Jobs</a>
                   </div>
                   <Profile />
                 </div>
@@ -47,7 +46,7 @@ const Header = () => {
               <>
                 <div className="hidden md:flex flex-col items-center md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
                   <div className='mr-5'>
-                    <a href="#" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-[#12D465] rounded hover:bg-[#F0F4F8] hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">All Jobs</a>
+                    <a href="#" className="p-3 lg:px-8 md:mx-4 text-black text-centerx bg-green-500 rounded hover:bg-gray-100 hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1">All Jobs</a>
                   </div>
                   <Profile />
                 </div>
